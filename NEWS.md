c6bc983c90620d61db1794d8c73602a034198462
Colin Millar <colinpmillar@gmail.com>
2023-09-18 18:02:45 +0200

    fix check errors and warnings

--- NEWS.md
+++ NEWS.md
@@ -1,34 +1,23 @@
+# rfultils 0.0.3 (16/07/20)
 
-# v6_9
-# 22/06/20 drop ... from argument list to force criterion to pick up information from model object
+o ensure start, when supplied, is only given to the first stage of the optimisation (glmer)
 
-# 12/09/19 retainModels = FALSE only returns the summary table (useful when models are large)
+# rfutils 0.0.2 (22/06/20)
 
-# 11/09/19 catch for errors in model fits
+o 22/06/20 drop ... from argument list to force criterion to pick up information from model object
+o 12/09/19 retainModels = FALSE only returns the summary table (useful when models are large)
+o 11/09/19 catch for errors in model fits
+o 10/09/19 fitModels = FALSE returns the candidate models without fitting them
+o 10/09/19 allows for 2D smoothers - see notes for explanation of how to use
+o 29/03/17 allows more general likelihood calculation
+o 14/02/17 fix bug when smoothTerms is missing; also deal with random effects in the formula (lme4)
+o 01/01/17 essential argument allows terms to always be present
+o 01/01/17 by argument allows the by terms to be expanded when the by variable is continuous
+o 22/12/16 correct bug when picking up AIC for gamm4 models
+o 21/12/16 adjust for gamm models
 
-# 10/09/19 fitModels = FALSE returns the candidate models without fitting them
+# rfutils 0.0.1
 
-# 10/09/19 allows for 2D smoothers - see notes for explanation of how to use
-
-# 29/03/17 allows more general likelihood calculation
-
-# 14/02/17 fix bug when smoothTerms is missing; also deal with random effects in the formula (lme4)
-
-# 01/01/17 essential argument allows terms to always be present
-
-# 01/01/17 by argument allows the by terms to be expanded when the by variable is continuous
-
-# 22/12/16 correct bug when picking up AIC for gamm4 models
-
-
-# 21/12/16 adjust for gamm models
-
-
-# mygamm4 news
-
-# 11/08/19 add call as attribute of model object
-# 10/09/19 mer_only argument to return only the mer object (saves time and space!)
-# 11/09/19 allow calc.derivs to be passed in control argument (saves time)
-
-# 1_4
-# 16/07/20 ensure start, when supplied, is only given to the first stage of the optimisation (glmer)
+o 11/08/19 add call as attribute of model object
+o 10/09/19 mer_only argument to return only the mer object (saves time and space!)
+o 11/09/19 allow calc.derivs to be passed in control argument (saves time)
--- R/logLik_gam.R
+++ R/logLik_gam.R
@@ -1,95 +1,89 @@
 #' logLik_gam
 #'
 #' Log likelihood function for use in model selection of mixed models using gam or bam
-#' Here’s a function for extracting the log likelihood and (harder) the degrees of freedom 
-#' out of a gam or bam model.  The degrees of freedom are the number of fixed effects plus 
-#' the number of variances (not the edf as is usually reported by gam) and assumes the 
+#' Here's a function for extracting the log likelihood and (harder) the degrees of freedom
+#' out of a gam or bam model.  The degrees of freedom are the number of fixed effects plus
+#' the number of variances (not the edf as is usually reported by gam) and assumes the
 #' models are fitted by ML.
-#' 
-#' The function has been tested with a range of smoothers, but as good check would involve 
-#' fitting models in gam and also in gamm4 and check the df are the same. 
+#'
+#' The function has been tested with a range of smoothers, but as good check would involve
+#' fitting models in gam and also in gamm4 and check the df are the same.
 #'
 #' @param model detail
 #'
-#' @references
-#'
-#' @details
-#'
 #' @export
-
-
 logLik_gam <- function(model) {
-  
-  # calculates maximum likelihood from a gam or bam model 
+
+  # calculates maximum likelihood from a gam or bam model
   # the tricky part is getting the degrees of freedom right across
-  # different types of smoother  
-  
+  # different types of smoother
+
   stopifnot(
     class(model)[1] %in% c("gam", "bam"),
     model$method == "ML"
   )
-  
+
   log_likelihood <- - model$gcv.ubre
   attributes(log_likelihood) <- NULL
-  
-  
+
+
   # fixed df
   # rank gives total number of 'covariates'
   # df_smooth gives those columns that are not fixed effects
-  
+
   # tensor smooths have component fx which can have length greater than 1
-  #    will assume either all are fixed or none are fixed 
+  #    will assume either all are fixed or none are fixed
   # other standard smooths have component fixed which is length 1
-  
+
   if (length(model$smooth) == 0) {
     df_smooth <- 0
   } else {
-    
+
     df_smooth <- sapply(model$smooth, function(x) {
-      
+
       # is the smooth penalised?
-      
+
       if ("tensor.smooth" %in% class(x)) {
-        
+
         if (any(x$fx) && any(!x$fx)) {
           stop("not coded yet")
         }
-        
+
         unpenalised <- all(x$fx)
-        
+
       } else {
-        
+
         if (length(x$fixed) > 1) {
           stop("not coded yet")
         }
-        
+
         unpenalised <- x$fixed
-        
+
       }
-      
+
       if (unpenalised) {
         return(0)
-      } 
-      
-      x$df - x$null.space.dim  
-      
+      }
+
+      x$df - x$null.space.dim
+
     })
-    
+
   }
-  
+
   df_fixed <- model$rank - sum(df_smooth)
-  
-  
+
+
   # random df
   # smoothing paramters and scale estimate
-  
+
   df_random <- length(model$sp) + model$scale.estimated
-  
+
   df <- df_fixed + df_random
-  
+
   attributes(log_likelihood) <- list(df = df)
-  
+
   class(log_likelihood) <- "logLik"
-  
+
   log_likelihood
 }
